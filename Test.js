082e1380926ae64a05b08fe7f9fadd585b564365
Akhil <akhilmaurya8003@gmail.com>
2025-12-25 10:24:25 +0530

    var-Let-const notes updated

--- Test.js
+++ Test.js
@@ -1,1 +1,16 @@
 console.log("Akhil Maurya")
+
+const accountId = 12345
+let accountName = "Akhil"
+var accountPassword = 12345656
+accountCity = "Gurugram"
+
+let accountState
+
+console.log(accountId)
+/*
+    Prefer not to use var because of issue in block and function scope.
+
+    const value cannot be change
+*/
+console.table([accountId,accountName,accountPassword,accountCity,accountState])
