10b11654befed9fd12cfb803072a80ed72b9fb0a
Akhil <akhilmaurya8003@gmail.com>
2025-12-29 00:19:35 +0530

    Added Datatype in js

--- DataTypes.js
+++ DataTypes.js
@@ -4,3 +4,26 @@
 let a = 5
 let b 
 console.log(a + b)
+
+
+let name = "Akhil" // String
+let age = 26 // Int
+let isLoggedIn = false // Boolean
+
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean
+// null ==> empty value
+// undefined ==> value not defined
+// symbol => unique
+
+
+// Object
+
+console.log(typeof "Akhil")
+
+console.log(typeof null) // Object
+
+console.log(typeof undefined) //undefined
