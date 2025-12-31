8684f919be92ae441ef546ea0331ed6978724a8b
Akhil <akhilmaurya8003@gmail.com>
2025-12-31 22:53:13 +0530

    Added DataType conversion

--- DataType_conversion.js
+++ DataType_conversion.js
@@ -1,6 +1,20 @@
 let score = "65"
 
-console.log(typeof score)
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let numericScore = Number(score)
-console.log(typeof numericScore)
+// console.log(typeof numericScore)
+// console.log(numericScore)
+
+
+let isLoggedIn = 1
+
+console.log(typeof isLoggedIn)
+console.log(isLoggedIn)
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 0 & 1 represent false and true
